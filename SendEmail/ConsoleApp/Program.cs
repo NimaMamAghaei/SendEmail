9821f86b07f0342be4170815441cf94818a27654
Nima Aghaei <88058268+nimaMamAghaei@users.noreply.github.com>
2023-05-02 02:24:47 +0330

    Add Project
    For Send Email

---
+++ SendEmail/ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿try
+{
+    var smtpClient = new System.Net.Mail.SmtpClient
+    {
+        Credentials = new System.Net.NetworkCredential(userName: "Your Gmail", password: "App Password Code"), // Use App Password Generated instead of Password
+        Host = "smtp.gmail.com",
+        Port = 587,
+        EnableSsl = true,
+    };
+
+    var smtpSender = new FluentEmail.Smtp.SmtpSender(smtpClient: smtpClient);
+
+    FluentEmail.Core.Email.DefaultSender = smtpSender;
+
+    var response = await FluentEmail.Core.Email
+        .From("Your Gamil")
+        .To("Gmail recipient")
+        .Subject("Subject")
+        .Body("Messages")
+        .SendAsync();
+
+
+    if (response.Successful)
+    {
+        System.Console.WriteLine(value: "Email sent..");
+    }
+}
+catch (System.Exception exception)
+{
+    System.Console.WriteLine(value: exception.Message);
+}
